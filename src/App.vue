f89d151a1abb99fec48be3a82408464f3d0176d3
Paul Arizpe <kiramishima@outlook.com@>
2020-11-26 18:05:00 -0600

    tailcsswind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <main class="w-screen h-screen flex flex-col">
+    <medium-editor></medium-editor>
+  </main>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Editor from '@/components/Editor.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+@Options({
+    name: 'App',
+    components: {
+      'medium-editor': Editor
     }
-  }
+})
+export default class App extends Vue {
 }
-</style>
+</script>
---
+++ src/components/Editor.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="medium-editor-container" v-if="!readOnly">
+        <div class="editor" ref="editor">
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import MediumEditor from 'medium-editor';
+
+@Options({
+    name: 'Editor',
+    props: {
+        editor: MediumEditor
+    }
+})
+export default class Editor extends Vue {
+  editor!: MediumEditor.MediumEditor;
+  mounted() {
+    console.log("Mounted")
+    this.editor = new MediumEditor((this.$refs.editor as HTMLDivElement));
+  }
+}
+</script>
